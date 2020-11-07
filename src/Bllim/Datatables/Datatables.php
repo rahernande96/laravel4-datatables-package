742af0cd4f45b6e7989161cf5a7804977ca8579c
Robert Hernandez <rahernande96@gmail.com>
2020-11-07 18:50:47 -0400

    fix

--- src/Bllim/Datatables/Datatables.php
+++ src/Bllim/Datatables/Datatables.php
@@ -12,7 +12,7 @@
  */
 
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Arr;
@@ -64,7 +64,7 @@ class Datatables
     public function __construct()
     {
 
-        $this->setData($this->processData(Input::get()));
+        $this->setData($this->processData(Request::input()));
 
         return $this;
     }
